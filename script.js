e8a5ebbeab5bc32e65e271b4e320214931b870d1
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-08-06 18:11:18 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Your code here.
+document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelector(".items");
+  let isDragging = false;
+  let startX;
+  let scrollLeft;
+
+  items.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - items.offsetLeft;
+    scrollLeft = items.scrollLeft;
+    items.classList.add("active");
+  });
+
+  items.addEventListener("mouseleave", () => {
+    isDragging = false;
+    items.classList.remove("active");
+  });
+
+  items.addEventListener("mouseup", () => {
+    isDragging = false;
+    items.classList.remove("active");
+  });
+
+  items.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - items.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust the dragging speed
+    items.scrollLeft = scrollLeft - walk;
+  });
+});
+
